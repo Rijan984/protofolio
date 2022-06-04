8c89d42c32dba7d72246ccd1c0658e0195f59062
Rijan984 <rijan18shrestha@gmail.com>
2022-06-04 23:19:15 +0545

    hamro dukan

--- src/components/templates/Works/data.js
+++ src/components/templates/Works/data.js
@@ -1,33 +1,33 @@
-import himalayan from "../../images/himalayanJavaCoffee.JPG";
-import MCQ from "../../images/mcq.JPG";
-import hamroDukan from "../../images/hamroDukan.JPG";
+import himalayan from '../../images/himalayanJavaCoffee.JPG';
+import MCQ from '../../images/mcq.JPG';
+import hamroDukan from '../../images/hamroDukan.JPG';
 
 const data = [
   {
     id: 1,
     image: hamroDukan,
-    title: "Gharelu Kaam",
+    title: 'Hamro Dukan',
     description:
-      "This is the ecomerce website which was done using Reactjs, Redux with API.",
-    link: "",
-    github: "https://github.com/Rijan984/hamroDukan.git",
+      'This is the ecomerce website which was done using Reactjs, Redux with API.',
+    link: '',
+    github: 'https://github.com/Rijan984/hamroDukan.git',
   },
   {
     id: 2,
     image: himalayan,
-    title: "Himalayan Java Coffee",
+    title: 'Himalayan Java Coffee',
     description:
-      "This is the task done using HTML and CSS which was given by pagoda lab while applying for the job.",
-    link: "https://rijan984.github.io/padodaLab-himalaynJavaCoffee-/",
-    github: "",
+      'This is the task done using HTML and CSS which was given by pagoda lab while applying for the job.',
+    link: 'https://rijan984.github.io/padodaLab-himalaynJavaCoffee-/',
+    github: '',
   },
   {
     id: 3,
     image: MCQ,
-    title: "MCQ",
-    description: "This is the task done using React and Redux.",
-    link: "https://rijan984.github.io/MCQ/",
-    github: "https://github.com/Rijan984/MCQ.git",
+    title: 'MCQ',
+    description: 'This is the task done using React and Redux.',
+    link: 'https://rijan984.github.io/MCQ/',
+    github: 'https://github.com/Rijan984/MCQ.git',
   },
 ];
 
